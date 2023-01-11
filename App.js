bf5d1d78c76aa0b39cf72dd9c88109853c4d4a08
Mtuozzo86 <Mtuozzo86@gmail.com>
2023-01-11 15:15:22 -0500

    cant line up description

--- App.js
+++ App.js
@@ -1,33 +1,10 @@
 import { StatusBar } from "expo-status-bar";
-import {
-  StyleSheet,
-  Text,
-  View,
-  ActivityIndicator,
-  Pressable,
-  Animated,
-  FlatList,
-} from "react-native";
+import { StyleSheet, Text, View, Pressable, FlatList } from "react-native";
 import Ionicons from "react-native-vector-icons/Ionicons";
 import { useState } from "react";
 
 export default function App() {
   const [beers, setBeers] = useState([]);
-  const rando = beers.map((beer) => {
-    return (
-      <Pressable
-        style={{ textAlign: "center", textAlign: "center", width: "80%" }}
-        key={beer.id}
-        onPress={() => setBeers([])}
-      >
-        <Text style={{ fontWeight: 900, fontSize: 38 }}>{beer.name}</Text>
-        <Text>ABV: {beer.id}</Text>
-        <Text style={{ fontStyle: "italic", marginTop: 16 }}>
-          {beer.description}
-        </Text>
-      </Pressable>
-    );
-  });
 
   function getRandomBeer() {
     fetch("https://api.punkapi.com/v2/beers/random")
@@ -35,27 +12,23 @@ export default function App() {
       .then((data) => setBeers(data));
   }
 
+  function renderStuff({ item }) {
+    return (
+      <View style={styles.descriptionContainer}>
+        <Pressable style={styles.description} onPress={() => setBeers([])}>
+          <Text style={{ fontSize: 38 }}>{item.name}</Text>
+          <Text style={{ fontStyle: "italic", marginTop: 16 }}>
+            {item.description}
+          </Text>
+        </Pressable>
+      </View>
+    );
+  }
+
   return (
     <View style={styles.container}>
       {beers.length ? (
-        <View style={styles.descriptionContainer}>
-          <FlatList
-            
-            data={beers}
-            renderItem={({ item }) => (
-              <Pressable
-                onPress={() => setBeers([])}
-                style={{
-                  textAlign: "center",
-                  width: "80%",
-                }}
-              >
-                <Text>{item.name}</Text>
-                <Text>{item.description}</Text>
-              </Pressable>
-            )}
-          />
-        </View>
+        <FlatList data={beers} renderItem={renderStuff} />
       ) : (
         <View style={styles.buttonContainer}>
           <Pressable style={styles.beerButton} onPress={() => getRandomBeer()}>
@@ -79,7 +52,6 @@ const styles = StyleSheet.create({
   buttonContainer: {
     width: 120,
     height: 120,
-    marginHorizontal: 60,
     borderWidth: 5,
     borderColor: "#0b0a07",
     borderRadius: 100,
@@ -91,6 +63,7 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
   descriptionContainer: {
-    marginTop: 100
+    borderWidth: 5,
   },
+  description: {},
 });
