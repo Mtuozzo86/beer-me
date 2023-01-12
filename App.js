211c2e08ed2e2fb14e8afe80c7d9e4276fa65c7e
Mtuozzo86 <Mtuozzo86@gmail.com>
2023-01-11 23:13:19 -0500

    finish refactor

--- App.js
+++ App.js
@@ -1,17 +1,14 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Pressable, FlatList } from "react-native";
-import Ionicons from "react-native-vector-icons/Ionicons";
-import { useState } from "react";
+import { StyleSheet, View } from "react-native";
+
 import Navbar from "./Navbar";
 import RandomBeer from "./RandomBeer";
 
 export default function App() {
-
   return (
     <View style={styles.container}>
       <Navbar />
       <RandomBeer />
-
       <StatusBar style="auto" />
     </View>
   );
@@ -23,5 +20,5 @@ const styles = StyleSheet.create({
     backgroundColor: "#f0ec57",
     alignItems: "center",
     justifyContent: "flex-start",
-  }
+  },
 });
