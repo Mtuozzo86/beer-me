f97b86753f33b218708f66610d366d132a5480cd
Mtuozzo86 <Mtuozzo86@gmail.com>
2023-01-11 22:11:14 -0500

    decent refactor

--- App.js
+++ App.js
@@ -3,50 +3,17 @@ import { StyleSheet, Text, View, Pressable, FlatList } from "react-native";
 import Ionicons from "react-native-vector-icons/Ionicons";
 import { useState } from "react";
 import Navbar from "./Navbar";
+import RandomBeer from "./RandomBeer";
 
 export default function App() {
-  const [beers, setBeers] = useState([]);
-
-  function getRandomBeer() {
-    fetch("https://api.punkapi.com/v2/beers/random")
-      .then((r) => r.json())
-      .then((data) => setBeers(data));
-  }
-
-  function renderStuff({ item }) {
-    return (
-      <Pressable style={styles.description} onPress={() => setBeers([])}>
-        <Text style={{ fontSize: 38 }}>{item.name}</Text>
-        <Text>ABV: {item.abv}</Text>
-        <Text style={{ fontStyle: "italic", marginTop: 16 }}>
-          {item.description}
-        </Text>
-      </Pressable>
-    );
-  }
 
   return (
-    
-      <View style={styles.container}>
+    <View style={styles.container}>
       <Navbar />
-        {beers.length ? (
-          <View style={styles.descriptionContainer}>
-            <FlatList data={beers} renderItem={renderStuff} />
-          </View>
-        ) : (
-          <View style={styles.buttonContainer}>
-            <Pressable
-              style={styles.beerButton}
-              onPress={() => getRandomBeer()}
-            >
-              <Ionicons name="beer" size={70} color="#0b0a07" />
-            </Pressable>
-          </View>
-        )}
+      <RandomBeer />
 
-        <StatusBar style="auto" />
-      </View>
-    
+      <StatusBar style="auto" />
+    </View>
   );
 }
 
@@ -56,27 +23,5 @@ const styles = StyleSheet.create({
     backgroundColor: "#f0ec57",
     alignItems: "center",
     justifyContent: "flex-start",
-  },
-  buttonContainer: {
-    flex:1,
-    justifyContent: 'center'
-
-  },
-  beerButton: {
-    justifyContent: "center",
-    alignItems: "center",
-    width: 120,
-    height: 120,
-    borderWidth: 5,
-    borderColor: "#0b0a07",
-    borderRadius: 100,
-  },
-  descriptionContainer: {
-    flex: 1,
-    borderWidth: 1,
-    // width: 300,
-    padding: 10,
-  },
-  description: {},
+  }
 });
-// refactor
